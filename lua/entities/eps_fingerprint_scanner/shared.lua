0f35295da504d0ac00b92ae093c8f3beb55c80a0
Brian Mak <eggrollbot@gmail.com>
2019-12-21 22:28:01 -0800

    Changed category names for the scripted entities
    Changed the category names for the fingerprint scanner and the police computer from "Eggroll Police System" to "Eggroll's Police System".

--- lua/entities/eps_fingerprint_scanner/shared.lua
+++ lua/entities/eps_fingerprint_scanner/shared.lua
@@ -1,7 +1,7 @@
 ENT.Base = "base_gmodentity"
 ENT.Type = "anim"
 ENT.PrintName = "Fingerprint Scanner"
-ENT.Category = "Eggroll Police System"
+ENT.Category = "Eggroll's Police System"
 ENT.Author = "TheAsian EggrollMaker"
 ENT.Contact = "theasianeggrollmaker@gmail.com"
 ENT.Purpose = ""
