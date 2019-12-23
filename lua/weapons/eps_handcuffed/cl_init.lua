7e223c0dc966e04b0a3b7e524a979d998c0d1d2c
Brian Mak <eggrollbot@gmail.com>
2019-12-22 16:11:26 -0800

    Reworked eps_handcuffed
    Reworked eps_handcuffed due to a bug (PLAYER.EPS_Handcuffed was desynced on the client and server) that required a fundamental change. Most code was moved to the serverside and instead of having a variable on the player be shared, a NWBool is used instead.

--- lua/weapons/eps_handcuffed/cl_init.lua
+++ lua/weapons/eps_handcuffed/cl_init.lua
@@ -6,9 +6,3 @@ end
 function SWEP:PreDrawViewModel( )
 	return true
 end
-
-function SWEP:OwnerChanged( )
-	if IsValid( self:GetOwner( ) ) then -- Make sure it was picked up and not dropped.
-		self:Deploy( )
-	end
-end
--- lua/weapons/eps_handcuffed/init.lua
+++ lua/weapons/eps_handcuffed/init.lua
@@ -2,10 +2,65 @@ AddCSLuaFile( "cl_init.lua" )
 AddCSLuaFile( "shared.lua" )
 include( "shared.lua" )
 
-function SWEP:OwnerChanged( )
-	if IsValid( self:GetOwner( ) ) then -- Make sure it was picked up and not dropped.
-		self:GetOwner( ):SelectWeapon( self:GetClass( ) )
-	end
+function SWEP:Equip( ply )
+	ply:SelectWeapon( self:GetClass( ) )
+	self:StartHandcuffing( )
+end
+
+function SWEP:StartHandcuffing( )
+	local ply = self:GetOwner( )
+	ply:SetNWBool( "EPS_Handcuffed", true )
+	ply:ManipulateBoneAngles( ply:LookupBone( "ValveBiped.Bip01_L_UpperArm" ), Angle( 20, 18.8, 0 ) )
+	ply:ManipulateBoneAngles( ply:LookupBone( "ValveBiped.Bip01_L_Forearm" ), Angle( 32, 50, 10 ) )
+	ply:ManipulateBoneAngles( ply:LookupBone( "ValveBiped.Bip01_L_Hand" ), Angle( 0, 0, 165 ) )
+	ply:ManipulateBoneAngles( ply:LookupBone( "ValveBiped.Bip01_L_Finger0" ), Angle( -10, 10, 0 ) )
+	ply:ManipulateBoneAngles( ply:LookupBone( "ValveBiped.Bip01_L_Finger01" ), Angle( 0, 30, 0 ) )
+	ply:ManipulateBoneAngles( ply:LookupBone( "ValveBiped.Bip01_L_Finger1" ), Angle( 5, 90, 5 ) )
+	ply:ManipulateBoneAngles( ply:LookupBone( "ValveBiped.Bip01_L_Finger11" ), Angle( 0, 40, 0 ) )
+	ply:ManipulateBoneAngles( ply:LookupBone( "ValveBiped.Bip01_L_Finger2" ), Angle( -10, 90, 0 ) )
+	ply:ManipulateBoneAngles( ply:LookupBone( "ValveBiped.Bip01_L_Finger21" ), Angle( 0, 40, 0 ) )
+	ply:ManipulateBoneAngles( ply:LookupBone( "ValveBiped.Bip01_R_UpperArm" ), Angle( -20, 6.6, 30 ) )
+	ply:ManipulateBoneAngles( ply:LookupBone( "ValveBiped.Bip01_R_Forearm" ), Angle( -30, 90, 0 ) )
+	ply:ManipulateBoneAngles( ply:LookupBone( "ValveBiped.Bip01_R_Hand" ), Angle( 10, 0, -80 ) )
+	ply:ManipulateBoneAngles( ply:LookupBone( "ValveBiped.Bip01_R_Finger1" ), Angle( -20, 30, 0 ) )
+	ply:ManipulateBoneAngles( ply:LookupBone( "ValveBiped.Bip01_R_Finger11" ), Angle( 0, 30, 0 ) )
+	ply:ManipulateBoneAngles( ply:LookupBone( "ValveBiped.Bip01_R_Finger2" ), Angle( 0, 30, 0 ) )
+	ply:ManipulateBoneAngles( ply:LookupBone( "ValveBiped.Bip01_R_Finger21" ), Angle( 0, 90, 0 ) )
+	ply:ManipulateBoneAngles( ply:LookupBone( "ValveBiped.Bip01_R_Finger22" ), Angle( 0, 60, 0 ) )
+	ply.EPS_OldWalkSpeed = ply:GetWalkSpeed( )
+	ply.EPS_OldRunSpeed = ply:GetRunSpeed( )
+	ply:SetWalkSpeed( 120 )
+	ply:SetRunSpeed( 120 )
+end
+
+function SWEP:FreeHandcuffs( ) -- Do not call this directly.
+	local ply = self:GetOwner( )
+	ply:SetNWBool( "EPS_Handcuffed", false ) -- nil won't change it on the client for some reason.
+	ply:ManipulateBoneAngles( ply:LookupBone( "ValveBiped.Bip01_L_UpperArm" ), Angle( 0, 0, 0 ) )
+	ply:ManipulateBoneAngles( ply:LookupBone( "ValveBiped.Bip01_L_Forearm" ), Angle( 0, 0, 0 ) )
+	ply:ManipulateBoneAngles( ply:LookupBone( "ValveBiped.Bip01_L_Hand" ), Angle( 0, 0, 0 ) )
+	ply:ManipulateBoneAngles( ply:LookupBone( "ValveBiped.Bip01_L_Finger0" ), Angle( 0, 0, 0 ) )
+	ply:ManipulateBoneAngles( ply:LookupBone( "ValveBiped.Bip01_L_Finger01" ), Angle( 0, 0, 0 ) )
+	ply:ManipulateBoneAngles( ply:LookupBone( "ValveBiped.Bip01_L_Finger1" ), Angle( 0, 0, 0 ) )
+	ply:ManipulateBoneAngles( ply:LookupBone( "ValveBiped.Bip01_L_Finger11" ), Angle( 0, 0, 0 ) )
+	ply:ManipulateBoneAngles( ply:LookupBone( "ValveBiped.Bip01_L_Finger2" ), Angle( 0, 0, 0 ) )
+	ply:ManipulateBoneAngles( ply:LookupBone( "ValveBiped.Bip01_L_Finger21" ), Angle( 0, 0, 0 ) )
+	ply:ManipulateBoneAngles( ply:LookupBone( "ValveBiped.Bip01_R_UpperArm" ), Angle( 0, 0, 0 ) )
+	ply:ManipulateBoneAngles( ply:LookupBone( "ValveBiped.Bip01_R_Forearm" ), Angle( 0, 0, 0 ) )
+	ply:ManipulateBoneAngles( ply:LookupBone( "ValveBiped.Bip01_R_Hand" ), Angle( 0, 0, 0 ) )
+	ply:ManipulateBoneAngles( ply:LookupBone( "ValveBiped.Bip01_R_Finger1" ), Angle( 0, 0, 0 ) )
+	ply:ManipulateBoneAngles( ply:LookupBone( "ValveBiped.Bip01_R_Finger11" ), Angle( 0, 0, 0 ) )
+	ply:ManipulateBoneAngles( ply:LookupBone( "ValveBiped.Bip01_R_Finger2" ), Angle( 0, 0, 0 ) )
+	ply:ManipulateBoneAngles( ply:LookupBone( "ValveBiped.Bip01_R_Finger21" ), Angle( 0, 0, 0 ) )
+	ply:ManipulateBoneAngles( ply:LookupBone( "ValveBiped.Bip01_R_Finger22" ), Angle( 0, 0, 0 ) )
+	ply:SetWalkSpeed( ply.EPS_OldWalkSpeed )
+	ply:SetRunSpeed( ply.EPS_OldRunSpeed )
+	ply.EPS_OldWalkSpeed = nil
+	ply.EPS_OldRunSpeed = nil
+end
+
+function SWEP:OnRemove( ) -- Call WEAPON:Remove( ) to get rid of the handcuffs.
+	self:FreeHandcuffs( )
 end
 
 hook.Add( "PlayerUse", "EPS_UseWhileHandcuffed", function( ply )
