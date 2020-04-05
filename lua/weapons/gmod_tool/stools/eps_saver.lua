4febbdf1cf949de827d80a3b313dac07478fd675
Brian Mak <eggrollbot@gmail.com>
2020-04-04 18:11:14 -0700

    Fixed a few issues with the saving tool
    - Fixed the language names for the saving tool.
    
    - Fixed how EggrollPoliceSystem.SaveDevice, EggrollPoliceSystem.UnsaveDevice, and EggrollPoliceSystem.UpdateSavedDevice was being called on both the client and the server.

--- lua/weapons/gmod_tool/stools/eps_saver.lua
+++ lua/weapons/gmod_tool/stools/eps_saver.lua
@@ -1,9 +1,9 @@
 if CLIENT then
-	language.Add( "tool.epssaver.name", "Eggroll Police System Saving Tool" )
-	language.Add( "tool.epssaver.desc", "Saves Eggroll Police System devices." )
-	language.Add( "tool.epssaver.left", "Save a device." )
-	language.Add( "tool.epssaver.right", "Unsave a device." )
-	language.Add( "tool.epssaver.reload", "Update a saved device's position and angle." )
+	language.Add( "tool.eps_saver.name", "Saving Tool" )
+	language.Add( "tool.eps_saver.desc", "Saves Eggroll Police System devices." )
+	language.Add( "tool.eps_saver.left", "Save a device." )
+	language.Add( "tool.eps_saver.right", "Unsave a device." )
+	language.Add( "tool.eps_saver.reload", "Update a saved device's position and angle." )
 end
 
 local saveable_devices = {
@@ -12,7 +12,7 @@ local saveable_devices = {
 }
 
 TOOL.Category = "Eggroll Police System"
-TOOL.Name = "#tool.epssaver.name"
+TOOL.Name = "#tool.eps_saver.name"
 
 TOOL.Information = {
 	{
@@ -28,8 +28,11 @@ TOOL.Information = {
 
 function TOOL:LeftClick( trace )
 	local ent = trace.Entity
-	if not saveable_devices[ class ] or ent:GetNWBool( "saved" ) then return false end
-	EggrollPoliceSystem.SaveDevice( ent )
+	if not saveable_devices[ ent:GetClass( ) ] or ent:GetNWBool( "saved" ) then return false end
+
+	if SERVER then
+		EggrollPoliceSystem.SaveDevice( ent )
+	end
 
 	return true
 end
@@ -37,7 +40,10 @@ end
 function TOOL:RightClick( trace )
 	local ent = trace.Entity
 	if not saveable_devices[ ent:GetClass( ) ] or not ent:GetNWBool( "saved" ) then return false end
-	EggrollPoliceSystem.UnsaveDevice( ent )
+
+	if SERVER then
+		EggrollPoliceSystem.UnsaveDevice( ent )
+	end
 
 	return true
 end
@@ -45,13 +51,16 @@ end
 function TOOL:Reload( trace )
 	local ent = trace.Entity
 	if not saveable_devices[ ent:GetClass( ) ] or not ent:GetNWBool( "saved" ) then return false end
-	EggrollPoliceSystem.UpdateSavedDevice( ent )
+
+	if SERVER then
+		EggrollPoliceSystem.UpdateSavedDevice( ent )
+	end
 
 	return true
 end
 
 function TOOL.BuildCPanel( cpanel )
 	cpanel:AddControl( "Header", {
-		Description = "#tool.epssaver.desc"
+		Description = "#tool.eps_saver.desc"
 	} )
 end
